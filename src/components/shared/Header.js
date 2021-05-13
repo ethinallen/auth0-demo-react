1303e35a0c36abe58125065b5dbc92c9c26f088c
Rudy Dahbura <rdahbura@gmail.com>
2021-05-13 15:34:11 -0700

    Updated title.

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
         <div className="container">
           <Link className="navbar-brand" to="/">
             <Logo />
-            Demo SPA
+            Demo
           </Link>
           <button
             className="navbar-toggler"
