0e47eb126057a79d29b3831e88b5052ab128e982
Rudy Dahbura <rdahbura@gmail.com>
2022-07-29 15:56:50 -0700

    Updated to createRoot.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import { Auth0Provider } from '@auth0/auth0-react';
 
@@ -31,13 +31,14 @@ const options = {
   onRedirectCallback
 };
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <Auth0Provider {...options}>
     <GlobalProvider>
       <App />
     </GlobalProvider>
-  </Auth0Provider>,
-  document.getElementById('root')
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
